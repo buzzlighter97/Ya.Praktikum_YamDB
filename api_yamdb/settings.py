62b748a36dcf47113c765effcf1192696d96c3e6
Ivan Ivanov <buzzlighter97@gmail.com>
2021-03-05 21:05:49 +0300

    commit

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -105,7 +105,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 STATIC_ROOT = os.path.join(
     BASE_DIR, "static/"
 )  # папка, в которой будет лежать статика
